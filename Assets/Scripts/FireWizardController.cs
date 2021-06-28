b6d388946552f88010538d99f5467aa64aa6d1d5
Isaac-Kua <71959139+Isaac-Kua@users.noreply.github.com>
2021-06-28 18:11:39 +0800

    [fix] rigidbody for firewizard

--- Assets/Scripts/FireWizardController.cs
+++ Assets/Scripts/FireWizardController.cs
@@ -21,78 +21,90 @@ public class FireWizardController : MonoBehaviour
 	private float distance;
 	private bool poofCharge = true;
 	private bool ammo = true;
+	private Vector2 dir;
+	private Quaternion angle = new Quaternion(0,0,0,0);
 
-    
+	
 	
 	// Start is called before the first frame update
-    void Start()
-    {
-        fireWizBody = GetComponent<Rigidbody2D>();
+	void Start()
+	{
+		fireWizBody = GetComponent<Rigidbody2D>();
 		fireWizSprite = GetComponent<SpriteRenderer>();
-    }
+	}
 
 	void FixedUpdate()
 	{
 		if (Input.GetKeyUp("f")){
 			Poof();
-		 }
-	}
-	
-    // Update is called once per frame
-    void Update()
-     {
-         distance = Vector2.Distance(transform.position, target1.transform.position);
- 
-         if (distance > maxRange)
-         {
-             transform.position = Vector2.MoveTowards(transform.position, target1.transform.position, speed * Time.deltaTime);
-         }
- 
-         else if (distance < minRange)
-         {
-			if (poofCharge) 
-			{
+		}
+		
+		if (Input.GetKeyUp("f")){
+			Fire();
+			Debug.Log(gameObject.tag);
+		}
+		
+		dir = (target1.transform.position - this.transform.position).normalized;
+		Vector3 eulerAngle = new Vector3(0,0,Vector2.SignedAngle(Vector2.right,dir));
+		angle.eulerAngles = eulerAngle;
+		
+		distance = Vector2.Distance(transform.position, target1.transform.position);
+		transform.rotation = angle;
+
+		if (distance > maxRange)
+		{
+			fireWizBody.velocity = (dir * speed);
+			
+		} else if (distance > minRange && distance < maxRange) {
+			fireWizBody.velocity = Vector2.zero;
+			if (ammo) {
+				Fire();
+			} 
+		} else if (distance < minRange){
+			if (poofCharge) {
 				Poof();
 			} else {
-				 transform.position = Vector2.MoveTowards(transform.position, target1.transform.position, -speed * Time.deltaTime);
+				fireWizBody.velocity = (-1*dir * speed);
 			}
-         } 
-		 
-		 else
-		 {
-			 if (ammo) 
-			 {
-				 Fire();
-			 }
-		 }
-	 }
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		gameObject.GetComponent<Bumblebee>().dir = dir;
+		gameObject.GetComponent<Bumblebee>().speed = speed;
+		gameObject.GetComponent<Bumblebee>().detectionRange = maxRange;
+		gameObject.GetComponent<Bumblebee>().npcBody = fireWizBody;
+	}
 	
 	void Poof()
-	 {
-		 transform.position = new Vector2(Random.Range(-xBound, xBound),Random.Range(-yBound, yBound));
-		 poofCharge = false;
-		 StartCoroutine(Panic());
-	 }
-	 
-	 IEnumerator Panic()
-	 {
+	{
+		transform.position = new Vector2(Random.Range(-xBound, xBound),Random.Range(-yBound, yBound));
+		poofCharge = false;
+		StartCoroutine(Panic());
+	}
+	
+	IEnumerator Panic()
+	{
 		fireWizSprite.material.color = new Color(0,0,1); //C# Deep Blue
 		yield return new WaitForSeconds(poofChargeTime);
 		poofCharge = true;
 		fireWizSprite.material.color = new Color(1,1,1); //C# White
-	 }
-	 
-	 void Fire() 
-	 {
+	}
+	
+	void Fire() 
+	{
 		ammo = false;
 		GameObject firebolt = Instantiate(missile, transform.position, transform.rotation);
 		firebolt.GetComponent<FireboltController>().target1 = target1;
 		firebolt.GetComponent<FireboltController>().speed = missileSpeed;
 		StartCoroutine(WindUp());
-	 }
-	 
-	 IEnumerator  WindUp()
-	 {
+	}
+	
+	IEnumerator  WindUp()
+	{
 		fireWizSprite.material.color = new Color(1,1,0.5f); //C# Yellow
 		yield return new WaitForSeconds(windUpTime);
 		fireWizSprite.material.color = new Color(1,0,0); //C# Red
