d51fd95d6832f0566a36a2cb8c12e187d4242f8c
mattwong <56360065+mattwong@users.noreply.github.com>
2021-07-20 17:46:19 +0800

    ui manager logic

--- Assets/Scripts/Screens/UprgradeScreenController.cs
+++ Assets/Scripts/Screens/UprgradeScreenController.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class UprgradeScreenController : MonoBehaviour
 {
+    public Button p1;
     public Button upgradeButton1;
     public Button upgradeButton2;
     public Button upgradeButton3;
     public Button upgradeButton4;
+    public Button condition1;
+    private EventSystem eventSystem;
 
     // Start is called before the first frame update
     void Start()
     {
         AltarManager.NextStage += beforeUpgradeScreen;
+        eventSystem = EventSystem.current;
     }
 
     void Update()
@@ -27,6 +32,7 @@ public class UprgradeScreenController : MonoBehaviour
 
     void beforeUpgradeScreen()
     {
+        GameManager.centralManagerInstance.increaseStage();
         foreach (Transform eachChild in transform)
         {
             if (eachChild.name == "before Upgrade Selection")
@@ -34,6 +40,8 @@ public class UprgradeScreenController : MonoBehaviour
                 eachChild.gameObject.SetActive(true);
             }                
         }
+        Time.timeScale = 0f;
+        eventSystem.SetSelectedGameObject(p1.gameObject, new BaseEventData(eventSystem));
     }
 
     void UpgradeScreen()
@@ -45,34 +53,40 @@ public class UprgradeScreenController : MonoBehaviour
                 eachChild.gameObject.SetActive(true);
             }                
         }
+        eventSystem.SetSelectedGameObject(upgradeButton1.gameObject, new BaseEventData(eventSystem));
     }
 
     public void Upgrade1()
     {
         Debug.Log("Upgrade 1 selected");
         upgradeButton1.interactable = false;
+        eventSystem.SetSelectedGameObject(upgradeButton1.gameObject, new BaseEventData(eventSystem));
     }
 
     public void Upgrade2()
     {
         Debug.Log("Upgrade 2 selected");
         upgradeButton2.interactable = false;
+        eventSystem.SetSelectedGameObject(p1.gameObject, new BaseEventData(eventSystem));
     }
 
     public void Upgrade3()
     {
         Debug.Log("Upgrade 3 selected");
         upgradeButton3.interactable = false;
+        eventSystem.SetSelectedGameObject(p1.gameObject, new BaseEventData(eventSystem));
     }
 
     public void Upgrade4()
     {
         Debug.Log("Upgrade 4 selected");
         upgradeButton4.interactable = false;
+        eventSystem.SetSelectedGameObject(p1.gameObject, new BaseEventData(eventSystem));
     }
 
-    public void PlayerNo()
+    public void PlayerNo(int player)
     {
+        GameManager.centralManagerInstance.chosenPlayer = player;
         foreach (Transform eachChild in transform)
         {
             if (eachChild.name == "before Upgrade Selection")
@@ -108,10 +122,12 @@ public class UprgradeScreenController : MonoBehaviour
                 eachChild.gameObject.SetActive(true);
             }                
         }
+        eventSystem.SetSelectedGameObject(condition1.gameObject, new BaseEventData(eventSystem));
     }
 
-    public void SelectCondition()
+    public void SelectCondition(Button button)
     {
+        GameManager.centralManagerInstance.currentCondition = button.name;
         foreach (Transform eachChild in transform)
         {
             if (eachChild.name == "after Upgrade Selection")
@@ -119,5 +135,6 @@ public class UprgradeScreenController : MonoBehaviour
                 eachChild.gameObject.SetActive(false);
             }                
         }
+        Time.timeScale = 1f;
     }
 }
