9c7f4307c4420c98409e079061dd6e3170ab49a1
mattwong <56360065+mattwong@users.noreply.github.com>
2021-08-09 22:44:21 +0800

    [feat] how to play screen

--- Assets/Scripts/Screens/HowToPlay.cs
+++ Assets/Scripts/Screens/HowToPlay.cs
@@ -24,9 +24,6 @@ public class HowToPlay : MonoBehaviour
             {
                 eachChild.gameObject.SetActive(true);
             }
-            else {
-                eachChild.gameObject.SetActive(false);
-            }
         }
     }
 }
